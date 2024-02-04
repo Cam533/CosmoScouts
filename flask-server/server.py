140ffe8a2e87a017aa769e62e17482bb9544bb75
anjanabegur <anjanab@seas.upenn.edu>
2024-02-03 23:06:08 -0500

    weather and heatmap inital files

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask import Flask, request, jsonify
+import asyncio
+import python_weather
+import asyncio
+import os
+import folium
+
 
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route('/api/weather', methods=['GET'])
+def weather():
+    location = request.args.get('location')
+    weather_info = asyncio.run(get_weather_conditions(location))
+    return jsonify(weather_info)
 
-if __name__ == "__main__":
+@app.route('/api/heatmap', methods=['POST'])
+def heatmap():
+    locations = request.json['locations']
+    heatmap_url = generate_heatmap(locations)
+    return jsonify({'heatmap_url': heatmap_url})
+
+if __name__ == '__main__':
     app.run(debug=True)
